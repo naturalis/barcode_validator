14eb927130633c1c403d8ab4c6675a77e26e7cef
Rutger Vos <rutgeraldo@gmail.com>
2023-11-01 12:43:31 +0100

    adding wrapper

---
+++ barcode-validator.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This is just a simple wrapper script around boldigger_cline
+# that sets up certain defaults.
+
+# The credentials should be in an .env file analogous to .env.example
+source .env
