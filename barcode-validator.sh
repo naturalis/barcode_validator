fdd3d701f50b2ca054e5b201785b94b2e01edccc
Rutger Vos <rutgeraldo@gmail.com>
2023-11-01 12:57:26 +0100

    adding implementation

--- barcode-validator.sh
+++ barcode-validator.sh
@@ -5,3 +5,28 @@
 
 # The credentials should be in an .env file analogous to .env.example
 source .env
+
+# Marker can be coi, its, rbcl, case sensitive
+MARKER=coi
+
+# Input FASTA file is first argument
+INFILE=$1
+
+# Output folder is second
+OUTFOLDER=$2
+
+# Batch size 
+BATCH=1
+
+# Modify as per: 
+# https://github.com/DominikBuchner/BOLDigger/tree/master#use-the-bold-identification-engine-for-coi-its-and-rbcl--matk
+if [[ $MARKER == 'coi' ]]; then
+	BATCH=50
+elif [[ $MARKER == 'its' ]]; then
+	BATCH=10
+elif [[ $MARKER == 'rbcl' ]]; then
+	BATCH=5
+fi
+
+# Do the incantation
+boldigger-cline ie_${MARKER} $USER $PASS $INFILE $OUTFOLDER $BATCH
