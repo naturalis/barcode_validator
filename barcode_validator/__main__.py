6c4ac7d505071ac6a87b48bb1d46f5ce61c6a9a6
Rutger Vos <rutgeraldo@gmail.com>
2024-08-19 14:19:47 +0200

    removed actions

--- barcode_validator/__main__.py
+++ barcode_validator/__main__.py
@@ -3,7 +3,6 @@ import yaml
 from barcode_validator.config import Config
 from barcode_validator.alignment import *
 from barcode_validator.taxonomy import *
-from barcode_validator.persistence import *
 from barcode_validator.result import DNAAnalysisResult
 
 
@@ -14,55 +13,43 @@ def get_tip_by_processid(process_id, tree):
     return None
 
 
-def main(fasta_file_path, bold_sheet, actions):
+def main(fasta_file_path, bold_sheet):
     logging.info(f"Starting analysis for file: {fasta_file_path}")
 
-    # Pre-fetch process ID => taxonomy mapping
-    if 'id' in actions:
-        ncbi_tree = read_ncbi_taxonomy(config.get('ncbi_taxonomy'))
-        if bold_sheet:
-            bold_tree = read_bold_taxonomy(bold_sheet)
-        else:
-            raise ValueError("ID action requested but no BOLD spreadsheet provided")
+    # Create taxonomy trees
+    ncbi_tree = read_ncbi_taxonomy(config.get('ncbi_taxonomy'))
+    bold_tree = read_bold_taxonomy(bold_sheet)
 
-    # Start reading the FASTA file
+    # Open the FASTA file and process each record
     with open(fasta_file_path, 'r') as file:
-        for record in SeqIO.parse(file, 'fasta'):
-            process_id = record.id.split('_')[0]
-            logging.info(f'Processing FASTA record {process_id}')
+        for process_id, record in parse_fasta(file):
 
-            # Lookup species from process_id
+            # Instantiate result object with process ID
+            logging.info(f'Processing FASTA record {process_id}')
+            result = DNAAnalysisResult(process_id)
+
+            # Lookup species name from process_id
             tip = get_tip_by_processid(process_id, bold_tree)
-            species = tip.name
+            result.species = tip.name
+            logging.info(f"Species: {result.species}")
+
+            # Lookup expected and observed higher taxon
             for node in bold_tree.root.get_path(tip):
                 if node.taxonomic_rank == config.get('level'):
-                    taxon = node.name
-            logging.info(f"Species: {species}")
+                    result.exp_taxon = node.name
+                    break
+            result.obs_taxon = run_seqid(record, ncbi_tree)
 
-            # Instantiate result object, unalign sequence
-            result = DNAAnalysisResult(process_id)
-            record = unalign_sequence(record)
+            # Compute sequence quality metrics
+            aligned_sequence = align_to_hmm(record)
+            logging.debug(f"Sequence aligned to HMM: {aligned_sequence.seq}")
+            amino_acid_sequence = translate_sequence(aligned_sequence)
+            result.stop_codons = get_stop_codons(amino_acid_sequence)
+            result.seq_length = marker_seqlength(aligned_sequence)
+            result.ambiguities = num_ambiguous(record)
 
-            # Do the ID check
-            if 'id' in actions:
-                result.exp_taxon = taxon
-                result.obs_taxon = run_seqid(record, ncbi_tree)
-                result.species = species
-
-            # Check if sequence has no early stop codons
-            if 'stops' in actions:
-                aligned_sequence = align_to_hmm(record)
-                logging.debug(f"Sequence aligned to HMM: {aligned_sequence.seq}")
-                amino_acid_sequence = translate_sequence(aligned_sequence)
-                result.stop_codons = get_stop_codons(amino_acid_sequence)
-                result.seq_length = marker_seqlength(aligned_sequence)
-                result.ambiguities = num_ambiguous(record)
-
-            # Persist the results to a database
-            if 'persist' in actions:
-                persist_result(result)
-            else:
-                print(result)
+            # Stringify the result
+            print(result)
 
     logging.info("Analysis completed")
 
@@ -74,8 +61,6 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--fasta_file", help="Path to the input FASTA file")
     parser.add_argument("-c", "--config_file", help="Path to the configuration YAML file")
     parser.add_argument("-b", "--bold_sheet", help="BOLD XLSX spreadsheet with Lab Sheet and Taxonomy tabs")
-    parser.add_argument("-a", "--action", help="Comma-separated list of actions, e.g. id,stops,persist",
-                        default='id,stops')
     parser.add_argument("-v", "--verbosity", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
                         help="Set the logging verbosity (default: INFO)")
     args = parser.parse_args()
@@ -95,8 +80,5 @@ if __name__ == "__main__":
         print(f"Error setting up logging: {e}")
         exit(1)
 
-    # Parse the actions
-    actions = args.action.split(',')
-
     # Run the main analysis
-    main(args.fasta_file, args.bold_sheet, actions)
+    main(args.fasta_file, args.bold_sheet)
