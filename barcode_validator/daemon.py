27b7a723261235047a5987670c2d1f783e060242
Rutger Vos <rutgeraldo@gmail.com>
2024-08-23 16:02:02 +0200

    Removed logging of valid config object

--- barcode_validator/daemon.py
+++ barcode_validator/daemon.py
@@ -211,7 +211,6 @@ def main(config_file, verbosity):
     config.load_config(config_file)
     config.setup_logging(verbosity)
     logging.info("*** Barcode Validator Daemon starting ***")
-    logging.info(config)
 
     # Set up the database connection for tracking PRs
     try:
