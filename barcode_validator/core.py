4ce11d802b1c3567158660eee41f7f28c2c120db
Rutger Vos <rutgeraldo@gmail.com>
2024-08-30 16:27:44 +0200

    Major refactoring: now uses BlastRunner, though some of the latter's methods have moved in here.

--- barcode_validator/core.py
+++ barcode_validator/core.py
@@ -1,92 +1,162 @@
+import io
 import logging
+import tarfile
 from typing import List, Optional
 from barcode_validator.config import Config
-from barcode_validator.taxonomy import read_ncbi_taxonomy, read_bold_taxonomy, get_tip_by_processid, run_localblast
+from barcode_validator.taxonomy import BlastRunner
 from barcode_validator.alignment import parse_fasta, align_to_hmm, translate_sequence, get_stop_codons, \
     marker_seqlength, num_ambiguous
 from barcode_validator.result import DNAAnalysisResult
 from Bio.SeqRecord import SeqRecord
 from Bio.Phylo.BaseTree import Tree
+from nbitk.Taxon import Taxon
+from nbitk.Phylo.NCBITaxdmp import Parser as NCBIParser
+from nbitk.Phylo.BOLDXLSXIO import Parser as BOLDParser
 
 
 class BarcodeValidator:
-    def __init__(self, config: Config):
+    def __init__(self):
         """
         Initialize the BarcodeValidator object.
-        :param config: An instance of the Config class
         """
-        self.config = config
         self.ncbi_tree: Optional[Tree] = None
         self.bold_tree: Optional[Tree] = None
 
-    def initialize(self) -> None:
+    def initialize(self, ncbi_taxonomy_dump: str, bold_xlsx_file: str) -> None:
         """
         Initialize the taxonomy trees. This is separate from __init__ to allow for lazy loading.
         :return: None
         """
         logging.info("Initializing taxonomy trees...")
-        self.ncbi_tree = read_ncbi_taxonomy(self.config.get('ncbi_taxonomy'))
-        self.bold_tree = read_bold_taxonomy(self.config.get('bold_sheet_file'))
+        self.ncbi_tree = NCBIParser(tarfile.open(ncbi_taxonomy_dump, "r:gz")).parse()
+        with open(bold_xlsx_file, 'rb') as file:
+            excel_data = io.BytesIO(file.read())
+            self.bold_tree = BOLDParser(excel_data).parse()
         logging.info("Initialization complete.")
 
-    def validate_fasta(self, fasta_file_path: str) -> List[DNAAnalysisResult]:
+    def validate_fasta(self, fasta_file_path: str, config: Config) -> List[DNAAnalysisResult]:
         """
         Validate a FASTA file of DNA sequences.
         :param fasta_file_path: A path to a FASTA file
+        :param config: A Config object
         :return: A list of DNAAnalysisResult objects
         """
         results = []
         for process_id, record, json_config in parse_fasta(fasta_file_path):
-            result = self.validate_record(process_id, record)
+            scoped_config = config.local_clone(json_config)
+            result = self.validate_record(process_id, record, scoped_config)
             results.append(result)
         return results
 
-    def validate_record(self, process_id: str, record: SeqRecord) -> DNAAnalysisResult:
+    def validate_record(self, process_id: str, record: SeqRecord, config: Config) -> DNAAnalysisResult:
         """
         Validate a single DNA sequence record.
         :param process_id: A process ID
         :param record: A Bio.SeqRecord object
+        :param config: A Config object
         :return: A DNAAnalysisResult object
         """
-
-        # Instantiate result object with process ID and calculate full sequence stats
         result = DNAAnalysisResult(process_id)
-        result.full_length = len(record.seq)
-        result.full_ambiguities = num_ambiguous(record)
-
-        # Lookup expected species and higher taxon, infer observed higher taxa from blast
-        sp = get_tip_by_processid(process_id, self.bold_tree)  # TODO: what if this fails?
-        if sp is None:
-            logging.warning(f"Process ID {process_id} not found in BOLD tree.")
-            result.error = f"{process_id} not in BOLD"
-            return result
-
-        # Can safely assume species is not None here
-        result.species = sp
-        logging.info(f"Species: {result.species}")
-        for node in self.bold_tree.root.get_path(result.species):
-            if node.taxonomic_rank == self.config.get('level'):
-                result.exp_taxon = node
-                break
-        obs_taxon = run_localblast(record, self.ncbi_tree, self.bold_tree, self.config)
-
-        # Handle BLAST failure
-        if obs_taxon is None:
-            logging.warning(f"Local BLAST failed for {process_id}")
-            result.error = f"Local BLAST failed for sequence '{record.seq}'"
-        else:
-            result.obs_taxon = obs_taxon
-
-        # Compute marker quality metrics
-        aligned_sequence = align_to_hmm(record, self.config)
-        if aligned_sequence is None:
-            logging.warning(f"Alignment failed for {process_id}")
-            result.error = f"Alignment failed for sequence '{record.seq}'"
-        else:
-            amino_acid_sequence = translate_sequence(aligned_sequence, self.config)
-            result.stop_codons = get_stop_codons(amino_acid_sequence)
-            result.seq_length = marker_seqlength(aligned_sequence)
-            result.ambiguities = num_ambiguous(aligned_sequence)
+        self.validate_sequence_quality(config, record, result)
+        self.validate_taxonomy(config, record, result)
 
         # Return the result object
         return result
+
+    def validate_taxonomy(self, config: Config, record: SeqRecord, result: DNAAnalysisResult) -> None:
+        """
+        Validate the taxonomy of a DNA sequence record.
+        :param config: A Config object
+        :param record: A Bio.SeqRecord object
+        :param result: A DNAAnalysisResult object
+        """
+
+        # Lookup expected species in BOLD tree
+        sp = self.get_tip_by_processid(result.process_id)
+        if sp is None:
+            logging.warning(f"Process ID {result.process_id} not found in BOLD tree.")
+            result.error = f"{result.process_id} not in BOLD"
+        else:
+
+            # Traverse BOLD tree to find the expected taxon at the specified rank
+            result.species = sp
+            logging.info(f"Species: {result.species}")
+            for node in self.bold_tree.root.get_path(result.species):
+                if node.taxonomic_rank == config.get('level'):
+                    result.exp_taxon = node
+                    break
+
+            # Run local BLAST to find observed taxon at the specified rank
+            constraint = self.build_constraint(sp, config.get('constrain'))
+            br = BlastRunner(config)
+            obs_taxon = br.run_localblast(record, constraint, config.get('level'))
+
+            # Handle BLAST failure
+            if obs_taxon is None:
+                logging.warning(f"Local BLAST failed for {result.process_id}")
+                result.error = f"Local BLAST failed for sequence '{record.seq}'"
+            else:
+                result.obs_taxon = obs_taxon
+
+    def get_tip_by_processid(self, process_id):
+        """
+        Get a tip from the BOLD tree by its 'processid' attribute.
+        :param process_id: A process ID
+        :return: A Taxon object
+        """
+        logging.info(f'Looking up tip by process ID: {process_id}')
+        for tip in self.bold_tree.get_terminals():
+            if process_id in tip.guids:
+                return tip
+        return None
+
+    def build_constraint(self, bold_tip: Taxon, rank: str) -> str:
+        """
+        Given a tip from the BOLD tree, looks up its path to the root, fetching the interior node at the specified
+        taxonomic rank. Then, traverses the NCBI tree to find the identically-named node at the same rank and returns
+        its taxon ID.
+        :param bold_tip: A Taxon object from the BOLD tree
+        :param rank: A taxonomic rank to constrain the search to
+        :return: An NCBI taxon ID
+        """
+
+        # Find the node at the specified taxonomic rank in the BOLD lineage that subtends the tip
+        logging.info(f"Going to traverse BOLD taxonomy for the {rank} to which {bold_tip} belongs")
+        bold_anc = next(node for node in self.bold_tree.root.get_path(bold_tip) if node.taxonomic_rank == rank)
+        logging.info(f"BOLD {bold_tip.taxonomic_rank} {bold_tip.name} is member of {rank} {bold_anc.name}")
+
+        # Find the corresponding node at the same rank in the NCBI tree
+        ncbi_anc = next(node for node in self.ncbi_tree.get_nonterminals()
+                        if node.name == bold_anc.name and node.taxonomic_rank == rank)
+
+        # Return or die
+        if ncbi_anc:
+            logging.info(f"Corresponding ncbi node is taxon:{ncbi_anc.guids['taxon']}")
+            return ncbi_anc.guids['taxon']
+        else:
+            raise ValueError(f"Could not find NCBI node for BOLD node '{bold_anc}'")
+
+    @classmethod
+    def validate_sequence_quality(cls, config: Config, record: SeqRecord, result: DNAAnalysisResult) -> None:
+        """
+        Validate the quality of a DNA sequence record.
+        :param config: A Config object
+        :param record: A Bio.SeqRecord object
+        :param result: A DNAAnalysisResult object
+        :return: None
+        """
+
+        # Instantiate result object with process ID and calculate full sequence stats
+        result.full_length = len(record.seq)
+        result.full_ambiguities = num_ambiguous(record)
+
+        # Compute marker quality metrics
+        aligned_sequence = align_to_hmm(record, config.get('hmm_file'))
+        if aligned_sequence is None:
+            logging.warning(f"Alignment failed for {result.process_id}")
+            result.error = f"Alignment failed for sequence '{record.seq}'"
+        else:
+            amino_acid_sequence = translate_sequence(aligned_sequence, config.get('translation_table'))
+            result.stop_codons = get_stop_codons(amino_acid_sequence)
+            result.seq_length = marker_seqlength(aligned_sequence)
+            result.ambiguities = num_ambiguous(aligned_sequence)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,114 +1,175 @@
 import pytest
-from unittest.mock import patch, MagicMock
-from Bio.Seq import Seq
+from unittest.mock import Mock, patch, mock_open
 from Bio.SeqRecord import SeqRecord
-from barcode_validator.core import BarcodeValidator
-from barcode_validator.result import DNAAnalysisResult
+from Bio.Seq import Seq
+from Bio.Phylo.BaseTree import Tree, Clade
 from nbitk.Taxon import Taxon
+from barcode_validator.config import Config
+from barcode_validator.result import DNAAnalysisResult
+from core import BarcodeValidator
 
 
 @pytest.fixture
 def mock_config():
-    with patch('barcode_validator.core.Config') as MockConfig:
-        config = MockConfig.return_value
-        config.get.side_effect = lambda x: {
-            'ncbi_taxonomy': 'path/to/ncbi_taxonomy',
-            'bold_sheet_file': 'path/to/bold_sheet',
-            'level': 'family'
-        }.get(x)
-        yield config
+    config = Mock(spec=Config)
+    config.get.side_effect = lambda key: {
+        'level': 'family',
+        'constrain': 'order',
+        'hmm_file': 'mock_hmm.hmm',
+        'translation_table': 1
+    }[key]
+    return config
 
 
 @pytest.fixture
-def barcode_validator(mock_config):
-    return BarcodeValidator(mock_config)
+def barcode_validator():
+    return BarcodeValidator()
 
 
 @pytest.fixture
-def mock_trees():
-    with patch('barcode_validator.core.read_ncbi_taxonomy') as mock_ncbi, \
-            patch('barcode_validator.core.read_bold_taxonomy') as mock_bold:
-        mock_ncbi.return_value = MagicMock()
-        mock_bold.return_value = MagicMock()
-        yield mock_ncbi.return_value, mock_bold.return_value
+def mock_trees(barcode_validator):
+    # Create mock NCBI and BOLD trees
+    mock_ncbi_tree = Mock(spec=Tree)
+    mock_bold_tree = Mock(spec=Tree)
+
+    # Set up the BOLD tree structure
+    mock_bold_root = Mock(spec=Taxon)
+    mock_bold_tree.root = mock_bold_root
+    mock_bold_tree.get_terminals = Mock()
+
+    barcode_validator.ncbi_tree = mock_ncbi_tree
+    barcode_validator.bold_tree = mock_bold_tree
+
+    return barcode_validator
 
 
-def test_init(barcode_validator, mock_config):
-    assert barcode_validator.config == mock_config
-    assert barcode_validator.ncbi_tree is None
-    assert barcode_validator.bold_tree is None
+def test_initialize(barcode_validator):
+    with patch('core.NCBIParser') as mock_ncbi_parser, \
+            patch('core.BOLDParser') as mock_bold_parser, \
+            patch('core.tarfile.open'), \
+            patch('builtins.open', mock_open(read_data=b'mock_data')):
+        mock_ncbi_parser.return_value.parse.return_value = Mock(spec=Tree)
+        mock_bold_parser.return_value.parse.return_value = Mock(spec=Tree)
+
+        barcode_validator.initialize('mock_ncbi.tar.gz', 'mock_bold.xlsx')
+
+        assert isinstance(barcode_validator.ncbi_tree, Mock)
+        assert isinstance(barcode_validator.bold_tree, Mock)
 
 
-def test_initialize(barcode_validator, mock_trees):
-    barcode_validator.initialize()
-    assert barcode_validator.ncbi_tree == mock_trees[0]
-    assert barcode_validator.bold_tree == mock_trees[1]
-
-
-@patch('barcode_validator.core.parse_fasta')
-def test_validate_fasta(mock_parse_fasta, barcode_validator):
+@patch('core.parse_fasta')
+def test_validate_fasta(mock_parse_fasta, barcode_validator, mock_config):
     mock_parse_fasta.return_value = [
-        ('process_id1', SeqRecord(Seq('ATCG'), id='seq1'), None),
-        ('process_id2', SeqRecord(Seq('GCTA'), id='seq2'), None)
+        ('process1', SeqRecord(Seq('ATCG'), id='seq1'), {}),
+        ('process2', SeqRecord(Seq('GCTA'), id='seq2'), {})
     ]
 
-    with patch.object(barcode_validator, 'validate_record') as mock_validate_record:
-        mock_validate_record.side_effect = [
-            DNAAnalysisResult('process_id1'),
-            DNAAnalysisResult('process_id2')
-        ]
-
-        results = barcode_validator.validate_fasta('dummy.fasta')
+    with patch.object(barcode_validator, 'validate_record', return_value=Mock(spec=DNAAnalysisResult)) as mock_validate:
+        results = barcode_validator.validate_fasta('mock.fasta', mock_config)
 
         assert len(results) == 2
-        assert all(isinstance(result, DNAAnalysisResult) for result in results)
-        assert mock_validate_record.call_count == 2
+        assert mock_validate.call_count == 2
 
 
-@patch('barcode_validator.core.get_tip_by_processid')
-@patch('barcode_validator.core.run_localblast')
-@patch('barcode_validator.core.align_to_hmm')
-@patch('barcode_validator.core.translate_sequence')
-@patch('barcode_validator.core.get_stop_codons')
-@patch('barcode_validator.core.marker_seqlength')
-@patch('barcode_validator.core.num_ambiguous')
-def test_validate_record(mock_num_ambiguous, mock_marker_seqlength, mock_get_stop_codons,
-                         mock_translate_sequence, mock_align_to_hmm, mock_run_localblast,
-                         mock_get_tip_by_processid, barcode_validator, mock_trees):
-    barcode_validator.ncbi_tree, barcode_validator.bold_tree = mock_trees
+def test_validate_record(mock_trees, mock_config):
+    record = SeqRecord(Seq('ATCG'), id='seq1')
 
-    mock_species = Taxon(name="Mock Species", taxonomic_rank="species")
-    mock_get_tip_by_processid.return_value = mock_species
-    mock_run_localblast.return_value = [Taxon(name="FamilyA", taxonomic_rank="family"),
-                                        Taxon(name="FamilyB", taxonomic_rank="family")]
-    mock_align_to_hmm.return_value = SeqRecord(Seq('ATCG' * 125), id='aligned_seq1')
-    mock_translate_sequence.return_value = SeqRecord(Seq('M' * 166), id='translated_seq1')
+    with patch.object(mock_trees, 'validate_sequence_quality') as mock_validate_quality, \
+            patch.object(mock_trees, 'validate_taxonomy') as mock_validate_taxonomy:
+        result = mock_trees.validate_record('process1', record, mock_config)
+
+        assert isinstance(result, DNAAnalysisResult)
+        mock_validate_quality.assert_called_once()
+        mock_validate_taxonomy.assert_called_once()
+
+
+def test_validate_taxonomy(mock_trees, mock_config):
+    record = SeqRecord(Seq('ATCG'), id='seq1')
+    result = DNAAnalysisResult('process1')
+
+    mock_species = Mock(spec=Taxon)
+    mock_species.name = "TestSpecies"
+    mock_exp_taxon = Mock(spec=Taxon)
+    mock_exp_taxon.name = "TestFamily"
+    mock_exp_taxon.taxonomic_rank = "family"
+    mock_obs_taxon = [Mock(spec=Taxon)]
+    mock_obs_taxon[0].name = "ObservedFamily"
+
+    mock_trees.bold_tree.root.get_path.return_value = [mock_exp_taxon, mock_species]
+
+    with patch.object(mock_trees, 'get_tip_by_processid', return_value=mock_species), \
+            patch.object(mock_trees, 'build_constraint', return_value='1234'), \
+            patch('core.BlastRunner') as MockBlastRunner:
+        MockBlastRunner.return_value.run_localblast.return_value = mock_obs_taxon
+
+        mock_trees.validate_taxonomy(mock_config, record, result)
+
+        assert result.species == mock_species
+        assert result.exp_taxon == mock_exp_taxon
+        assert result.obs_taxon == mock_obs_taxon
+        assert result.error is None
+
+    # Test error case when species is not found
+    with patch.object(mock_trees, 'get_tip_by_processid', return_value=None):
+        mock_trees.validate_taxonomy(mock_config, record, result)
+        assert result.error == "process1 not in BOLD"
+
+    # Test error case when BLAST fails
+    with patch.object(mock_trees, 'get_tip_by_processid', return_value=mock_species), \
+            patch.object(mock_trees, 'build_constraint', return_value='1234'), \
+            patch('core.BlastRunner') as MockBlastRunner:
+        MockBlastRunner.return_value.run_localblast.return_value = None
+
+        mock_trees.validate_taxonomy(mock_config, record, result)
+        assert result.error == "Local BLAST failed for sequence 'ATCG'"
+
+
+def test_build_constraint(mock_trees):
+    mock_bold_tip = Mock(spec=Taxon)
+    mock_bold_tip.name = 'TestSpecies'
+    mock_bold_tip.taxonomic_rank = 'species'
+
+    mock_bold_anc = Mock(spec=Taxon)
+    mock_bold_anc.name = 'TestOrder'
+    mock_bold_anc.taxonomic_rank = 'order'
+
+    mock_ncbi_anc = Mock(spec=Taxon)
+    mock_ncbi_anc.name = 'TestOrder'
+    mock_ncbi_anc.taxonomic_rank = 'order'
+    mock_ncbi_anc.guids = {'taxon': '1234'}
+
+    mock_trees.bold_tree.root.get_path.return_value = [mock_bold_anc, mock_bold_tip]
+    mock_trees.ncbi_tree.get_nonterminals.return_value = [mock_ncbi_anc]
+
+    result = mock_trees.build_constraint(mock_bold_tip, 'order')
+
+    assert result == '1234'
+
+
+@patch('core.align_to_hmm')
+@patch('core.translate_sequence')
+@patch('core.get_stop_codons')
+@patch('core.marker_seqlength')
+@patch('core.num_ambiguous')
+def test_validate_sequence_quality(mock_num_ambiguous, mock_marker_seqlength, mock_get_stop_codons,
+                                   mock_translate_sequence, mock_align_to_hmm, mock_config):
+    record = SeqRecord(Seq('ATCG'), id='seq1')
+    result = DNAAnalysisResult('process1')
+
+    mock_align_to_hmm.return_value = 'ATCG----'
+    mock_translate_sequence.return_value = 'M'
     mock_get_stop_codons.return_value = []
-    mock_marker_seqlength.return_value = 500
-    mock_num_ambiguous.side_effect = [0, 2]  # For full sequence and aligned sequence
+    mock_marker_seqlength.return_value = 4
+    mock_num_ambiguous.side_effect = [0, 0]
 
-    # Mock the get_path method of the bold_tree
-    mock_family = Taxon(name="MockFamily", taxonomic_rank="family")
-    barcode_validator.bold_tree.root.get_path.return_value = [
-        Taxon(name="MockKingdom", taxonomic_rank="kingdom"),
-        Taxon(name="MockPhylum", taxonomic_rank="phylum"),
-        Taxon(name="MockClass", taxonomic_rank="class"),
-        Taxon(name="MockOrder", taxonomic_rank="order"),
-        mock_family,
-        Taxon(name="MockGenus", taxonomic_rank="genus"),
-        mock_species
-    ]
+    BarcodeValidator.validate_sequence_quality(mock_config, record, result)
 
-    record = SeqRecord(Seq('ATCG' * 100), id='seq1')
-    result = barcode_validator.validate_record('process_id', record)
-
-    assert isinstance(result, DNAAnalysisResult)
-    assert result.process_id == 'process_id'
-    assert result.full_length == 400
+    assert result.full_length == 4
     assert result.full_ambiguities == 0
-    assert result.species == mock_species
-    assert result.exp_taxon == mock_family
-    assert [taxon.name for taxon in result.obs_taxon] == ['FamilyA', 'FamilyB']
     assert result.stop_codons == []
-    assert result.seq_length == 500
-    assert result.ambiguities == 2
+    assert result.seq_length == 4
+    assert result.ambiguities == 0
+
+
+if __name__ == '__main__':
+    pytest.main()
