29c6a275e4c861e391cfb092923d3aa63688e9cf
Rutger Vos <rutgeraldo@gmail.com>
2025-02-14 03:18:21 +0100

    added header

--- barcode_validator/__main__.py
+++ barcode_validator/__main__.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 from barcode_validator.cli import BarcodeValidatorCLI
 
+"""
+barcode_validator
+
+A command line tool for DNA Barcode Validation.
+
+This tool ingests FASTA/TSV sequence data, optionally merges CSV analytics and YAML configuration
+into each result, performs structural and/or taxonomic validation, and outputs results as TSV.
+Optionally, it can emit only valid sequences as FASTA.
+"""
+
 def main():
     cli = BarcodeValidatorCLI()
     cli.run()
