113419c10a2c8a495dd2a8c64d40aa4e2f2f2398
Rutger Vos <rutgeraldo@gmail.com>
2025-07-03 14:10:19 +0200

    feat: extend IDServiceFactory to support BOLD reference database

--- barcode_validator/idservices/factory.py
+++ barcode_validator/idservices/factory.py
@@ -2,13 +2,15 @@ from typing import Optional
 from nbitk.config import Config
 from barcode_validator.idservices.idservice import IDService
 from barcode_validator.constants import RefDB
-
+from barcode_validator.idservices.ncbi import NCBI
+from barcode_validator.idservices.bold import BOLD
 
 class IDServiceFactory:
 
     @staticmethod
     def create_idservice(config: Config, refdb: RefDB) -> Optional[IDService]:
-        from barcode_validator.idservices.ncbi import NCBI
         if refdb == RefDB.NCBI:
             return NCBI(config)
+        elif refdb == RefDB.BOLD:
+            return BOLD(config)
         return None
