827619a90b61492dfc185ee27c869d2ef6bc021f
Rutger Vos <rutgeraldo@gmail.com>
2025-07-18 23:49:10 +0200

    refactor: replace Config with SchemaConfig in test_bold_id.py

--- tests/test_bold_id.py
+++ tests/test_bold_id.py
@@ -1,15 +1,13 @@
 import pytest
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-from nbitk.config import Config
+from barcode_validator.config.schema_config import SchemaConfig
 from barcode_validator.idservices.bold import BOLD
 
 @pytest.fixture()
 def config():
     """Fixture to provide a Config object for tests."""
-    config = Config()
-    config.config_data = { "log_level": "DEBUG" }
-    config.initialized = True
+    config = SchemaConfig()
     return config
 
 
