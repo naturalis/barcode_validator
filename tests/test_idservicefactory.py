2ee5771b6af3080f93d6abd9c23df1fb12cd618d
Rutger Vos <rutgeraldo@gmail.com>
2025-07-18 23:49:24 +0200

    refactor: replace Config with SchemaConfig in test_idservicefactory.py

--- tests/test_idservicefactory.py
+++ tests/test_idservicefactory.py
@@ -1,19 +1,22 @@
 import pytest
 from barcode_validator.idservices.factory import IDServiceFactory
 from barcode_validator.constants import RefDB
-from nbitk.config import Config
+from barcode_validator.config.schema_config import SchemaConfig
 
 @pytest.fixture
 def config():
     """Fixture to provide a Config object for testing."""
-    config = Config()
-    config.config_data = { "reference_taxonomy": "ncbi", "log_level": "DEBUG" }
-    config.initialized = True
+    config = SchemaConfig()
+    config.set('log_level', 'DEBUG')
+    config.set('reflib_taxonomy_type', 'ncbi')
     return config
 
 def test_create_idservice_ncbi(config):
     """Test creating an NCBI ID service."""
-    db = RefDB(config.get('reference_taxonomy'))
+    db = RefDB(config.get('reflib_taxonomy_type'))
     id_service = IDServiceFactory.create_idservice(config, db)
     assert id_service is not None
     assert id_service.__class__.__name__ == "NCBI"
+
+if __name__ == '__main__':
+    pytest.main(['-v'])
