c80a628d1a3c246613b2434be81644f8b9fcb668
Rutger Vos <rutgeraldo@gmail.com>
2025-08-08 00:49:56 +0200

    docs: clean up command line argument examples in cli.py

--- barcode_validator/cli.py
+++ barcode_validator/cli.py
@@ -60,27 +60,6 @@ case the longest valid sequence in each group is retained.
 Configuration parameters can be set using command line arguments. For nested parameters, use the format:
 --section-name key=value
 
-For example:
-
-    python barcode_validator \
-      --input-file $INPUT_FILE \
-      # --csv-file $CSV_FILE \ # if you have CSV output from MGE it will join its lines with the input by sequence ID
-      # --yaml-file $YAML_FILE \ # if you have batch-level metadata in YAML format it will paste it across all output lines
-      --mode both \
-      --marker COI-5P \
-      --exp-taxonomy-type bold \
-      --exp-taxonomy $BOLD_EXCEL \
-      # --reflib-taxonomy-type bold \   # this is for when you have a local BLAST DB that needs to be mapped back to taxonomy tree, i.e. not needed for BOLD web service actually
-      # --reflib-taxonomy $BOLD_EXCEL \ # same, see one line above
-      --output-fasta $OUTPUT_FASTA \
-      --output-tsv $OUTPUT_TSV \
-      --taxon-validation method=bold \
-      --taxon-validation rank=family \
-      # --taxon-validation extent=class \ # this is for when you have a local BLAST DB that is taxonomically aware so that you can constrain the search space. Not needed for BOLD
-      --triage-config group_id_separator=_ \
-      --triage-config group_by_sample=true \
-      --log-level ERROR 2> out.log
-
 """,
             formatter_class=argparse.RawTextHelpFormatter
         )
