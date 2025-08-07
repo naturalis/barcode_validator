6b5cca1c8de2bcbc9e44001304b828ab4c50214e
Rutger Vos <rutgeraldo@gmail.com>
2025-08-07 13:56:52 +0200

    feat: update IDServiceFactory to use BLAST instead of NCBI for ID service creation, because this is about generic blasting rather than GenBank specific

--- barcode_validator/idservices/factory.py
+++ barcode_validator/idservices/factory.py
@@ -2,15 +2,15 @@ from typing import Optional
 from nbitk.config import Config
 from barcode_validator.idservices.idservice import IDService
 from barcode_validator.constants import RefDB
-from barcode_validator.idservices.ncbi import NCBI
+from barcode_validator.idservices.blast import BLAST
 from barcode_validator.idservices.bold import BOLD
 
 class IDServiceFactory:
 
     @staticmethod
     def create_idservice(config: Config, refdb: RefDB) -> Optional[IDService]:
-        if refdb == RefDB.NCBI:
-            return NCBI(config)
+        if refdb == RefDB.BLAST:
+            return BLAST(config)
         elif refdb == RefDB.BOLD:
             return BOLD(config)
         return None
