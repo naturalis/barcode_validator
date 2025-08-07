d8225bf421b85eea7229448f30a7499489748ac6
Rutger Vos <rutgeraldo@gmail.com>
2025-08-07 14:01:10 +0200

    feat: update run.sh to support additional output file options for fasta and tsv formats

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# Usage: ./run.sh <input_file>
+# Usage: ./run.sh <input_file> [<output_fasta> <output_tsv>]
 INPUT_FILE=$1
+OUTPUT_FASTA=${2:-out.fasta}
+OUTPUT_TSV=${3:-out.tsv}
 BOLD_EXCEL=tests/data/bold.xlsx
 
 python barcode_validator \
@@ -14,10 +16,11 @@ python barcode_validator \
   --exp-taxonomy $BOLD_EXCEL \
   # --reflib-taxonomy-type bold \   # this is for when you have a local BLAST DB that needs to be mapped back to taxonomy tree, i.e. not needed for BOLD web service actually
   # --reflib-taxonomy $BOLD_EXCEL \ # same, see one line above
-  --output-format tsv \
+  --output-fasta $OUTPUT_FASTA \
+  --output-tsv $OUTPUT_TSV \
   --taxon-validation method=bold \
   --taxon-validation rank=family \
   # --taxon-validation extent=class \ # this is for when you have a local BLAST DB that is taxonomically aware so that you can constrain the search space. Not needed for BOLD
   --triage-config group_id_separator=_ \
   --triage-config group_by_sample=true \
-  --log-level ERROR > out.tsv
+  --log-level ERROR 2> out.log
