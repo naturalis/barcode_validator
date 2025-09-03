2bcbda25d552b47cd226ef4272520de9d216c5b2
rvosa <rutgeraldo@gmail.com>
2025-09-03 07:28:25 +0200

    feat: extend IDServiceFactory to support BOLDDISTILLED reference database

--- barcode_validator/idservices/factory.py
+++ barcode_validator/idservices/factory.py
@@ -2,18 +2,21 @@ from typing import Optional
 from nbitk.config import Config
 from barcode_validator.idservices.idservice import IDService
 from barcode_validator.constants import RefDB
-from barcode_validator.idservices.blast import BLAST
-from barcode_validator.idservices.bold import BOLD
+from barcode_validator.idservices.blast import BLAST as LocalBLAST
+from barcode_validator.idservices.bold import BOLD as BOLDIDService
 from barcode_validator.idservices.galaxy_blast import GalaxyBLAST
+from barcode_validator.idservices.bolddistilled import BLAST as BOLDDistilled
 
 class IDServiceFactory:
 
     @staticmethod
     def create_idservice(config: Config, refdb: RefDB) -> Optional[IDService]:
         if refdb == RefDB.BLAST:
-            return BLAST(config)
+            return LocalBLAST(config)
         elif refdb == RefDB.BOLD:
-            return BOLD(config)
+            return BOLDIDService(config)
         elif refdb == RefDB.GALAXY:
             return GalaxyBLAST(config)
+        elif refdb == RefDB.BOLDDISTILLED:
+            return BOLDDistilled(config)
         return None
